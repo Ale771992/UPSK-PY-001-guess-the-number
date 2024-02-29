3d3330a6dc197abe23b1bda6930c837a7dcc16f7
Ale77$ <encuadernacionyali@gmail.com>
2024-02-29 10:30:46 -0600

    All tests up and running good

--- test_guess_thenumber.py
+++ test_guess_thenumber.py
@@ -1,35 +1,44 @@
 import unittest
 import random
-from main import main
+from unittest.mock import patch
 from user_guess import user_guess
 from computer_guess import computer_guess
 
 # unittest contains both a testing framework and a test runner
 # class se usa para definir un "molde" que contendra metodos de prueba
-class TestGuess(unittest.TestCase):#TestCase es una clase dentro de unittest que te da funcionalidades y metodos para escribir tus pruebas. 
+class TestAleatoryNumber(unittest.TestCase):#TestCase es una clase dentro de unittest que te da funcionalidades y metodos para escribir tus pruebas. 
  def test_random_number(self): # debes incluir el parámetro self, es necesario para acceder a las aserciones y otros métodos proporcionados por la clase TestCase.
   
   generated_number = random.randint(1, 100)
   
-  self.assertGreaterEqual(generated_number, 1, "Generated numero is too small")
-  self.assertLessEqual(generated_number, 100, "Generated number is too big")
+  self.assertGreaterEqual(generated_number, 1)
+  self.assertLessEqual(generated_number, 100)
+
 # Test para cuando el usuario adivina
- def test_guessuser_correct(self):
+class TestUserGuess(unittest.TestCase):
+ @patch('builtins.input', return_value ='50')
+ def test_guessuser_correct(self, mock_input):
    aleatory_number = 50
-   guessed_user = True
-   result = user_guess(aleatory_number, guessed_user)
-   print(result)
-   self.assertTrue(result)
+   guessed_user = False
+   self.assertTrue(user_guess(aleatory_number, guessed_user))
+
+ @patch('builtins.input', return_value = '25')
+ def test_guessuser_incorrect(self, mock_input):
+   aleatory_number = 50
+   guessed_user = False
+   self.assertFalse(user_guess(aleatory_number, guessed_user))
+
 
 #Test para cuando la computadora falla
- def test_guesscomputer_incorrect(self):
+class TestComputerGuess(unittest.TestCase):
+  @patch('builtins.input', return_value = '25')
+  def test_guesscomputer_incorrect(self, mock_input):
    aleatory_number = 50
-   result = computer_guess(aleatory_number)
-   print(result)
-   self.assertFalse(result)
+   guessed_computer = False
+   self.assertFalse(computer_guess(aleatory_number))
                            
 if __name__=='__main__':
-    unittest.main()
+   unittest.main()
 
     # unittest requires that: You put your tests into classes as methods and
     # You use a series of special assertion methods in the unittest.TestCase class instead of the built-in assert statement
--- user_guess.py
+++ user_guess.py
@@ -2,6 +2,7 @@ from colorama import init, Fore
 def user_guess(aleatory_number, guessed_user):
     # Con input le pedimos al usuario que ingrese un numero y se usa int para convertir la entrada a numero entero.
     guessed_user = False
+    # La función int() se encarga de convertir la entrada del usuario, que es inicialmente una cadena de texto (string), en un número entero.
     guess_user = int(input("Guess a number between 1 and 100: ----> "))
     print(Fore.LIGHTGREEN_EX + "Your guess is: ", guess_user)
     if guess_user == aleatory_number:
